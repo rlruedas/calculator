5c82d20e58d6aa31f256b0dad6e126d15c877ff4
rlruedas <rlruedas@student.apc.edu.ph>
2020-02-17 13:23:13 +0800

    qwe

---
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    double[] Button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+
+
+    }
+}
